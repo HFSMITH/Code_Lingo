1593f21bfbf1352d46187f60bddaa72465b160fb
Harry Fitzgerald-Smith <31471304+HFSMITH@users.noreply.github.com>
2019-12-08 17:04:39 +0000

    Valid Username check

---
+++ validusername.js
@@ -0,0 +1,14 @@
+function allLetter(inputtxt)
+  {
+   var letterNumber = /^[0-9a-zA-Z]+$/;
+   if(inputtxt.value.match(letters))
+     {
+      return true;
+     }
+   else
+     {
+     return false;
+     alert("The username must only contain alphabetical or numerical characters!")
+     }
+  }
+allLetter('helloworld!')
